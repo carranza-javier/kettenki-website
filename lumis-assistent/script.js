e6ef31e83ab2ae01e2f376c21edbeb2b740cfed3
jcarranza <kettenki@gmail.com>
2026-02-09 10:12:08 +0100

    Unmock

--- lumis-assistent/script.js
+++ lumis-assistent/script.js
@@ -15,7 +15,7 @@ const AUTH_CONFIG = {
     tokenExpKey: 'lumis_token_exp'
 };
 
-const MOCK_ENABLED = true;
+const MOCK_ENABLED = false;
 
 // [Rest of translations and configurations remain the same...]
 const translations = {
